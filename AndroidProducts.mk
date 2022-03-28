74d295608befc46b657f27817f38665ac11cdc6e
Cyder <87647653+CyderVII28x03@users.noreply.github.com>
2022-03-28 07:57:13 +0700

    star2lte: Awaken-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_star2lte.mk
+    $(LOCAL_DIR)/awaken_star2lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_star2lte-eng \
-    arrow_star2lte-user \
-    arrow_star2lte-userdebug
+    awaken_star2lte-eng \
+    awaken_star2lte-user \
+    awaken_star2lte-userdebug
